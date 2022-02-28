c49517b958f292a8c1273171f1078051eaa73f6e
Mike <masha256bit@protonmail.com>
2022-02-28 12:53:41 -0800

    mumbai and staging updated (#36)

---
+++ deploy/006_protocol_treasury.js
@@ -0,0 +1,36 @@
+
+const { isProtocolChain } = require("../test/_networks");
+const {
+    withConfirmation,
+    log,
+    deployWithConfirmation
+} = require("../utils/deploy");
+
+const deployProtocolTreasury = async () => {
+    const {deployerAddr, governorAddr} = await getNamedAccounts();
+    const sDeployer = await ethers.provider.getSigner(deployerAddr);
+
+    let result = await deployWithConfirmation('CaskTreasury');
+
+    const caskTreasury = await ethers.getContract("CaskTreasury");
+
+    result = await withConfirmation(
+        caskTreasury.connect(sDeployer).transferOwnership(governorAddr)
+    );
+    log(`Transferred CaskTreasury ownership to ${governorAddr}`, result);
+
+}
+
+
+const main = async (hre) => {
+    console.log("Running 006_protocol_treasury deployment...");
+    await deployProtocolTreasury(hre);
+    console.log("006_protocol_treasury deploy done.");
+    return true;
+};
+
+main.id = "006_protocol_treasury";
+main.tags = ["protocol"];
+main.skip = () => !isProtocolChain;
+
+module.exports = main;
