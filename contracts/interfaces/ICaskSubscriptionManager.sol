19cd7741a0e023d25eb40945de4e10ae289c8c26
mike <masha256bit@protonmail.com>
2022-03-22 14:57:43 -0700

    keeper overhaul and testnets deploy

--- contracts/interfaces/ICaskSubscriptionManager.sol
+++ contracts/interfaces/ICaskSubscriptionManager.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.0;
 
 interface ICaskSubscriptionManager {
 
+    enum CheckType {
+        None,
+        Active,
+        PastDue
+    }
+
     function processSinglePayment(address _consumer, address _provider, uint256 _subscriptionId, uint256 _value) external;
 
     function renewSubscription(uint256 _subscriptionId) external;
