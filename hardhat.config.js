a6907c1bbe89fd01f5b346b1c5f5a8c8183b0ced
mike <masha256bit@protonmail.com>
2022-03-31 11:17:53 -0700

    keeper updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -58,8 +58,9 @@ task("fixtures", "Setup fixtured provider plans and consumer subscriptions", fix
 task("keeper", "Run a keeper")
     .addOptionalParam("limit", "Max subscriptions to process per run", "4")
     .addOptionalParam("minDepth", "Only run keeper if queue is at least this deep", "0")
-    .addOptionalParam("queue", "1 for active queue, 2 for past due queue", "1")
-    .addOptionalParam("interval", "How often (in ms) to do keeper upkeep check", "10000")
+    .addOptionalParam("queue", "comma separated list of queues - 1 for active queue, 2 for past due queue", "1,2")
+    .addOptionalParam("interval", "How often (in ms) to do keeper upkeep check", "30000")
+    .addOptionalParam("gasLimit", "gasLimit for keeper transaction", "2500000")
     .setAction(keeper);
 
 module.exports = {
--- tasks/keeper.js
+++ tasks/keeper.js
@@ -1,25 +1,40 @@
 async function keeper(taskArguments, hre) {
 
-   console.log(`Keeper running with limit ${taskArguments.limit} on queue ${taskArguments.queue}`)
+    const queues = taskArguments.queue.split(/\s*,\s*/);
+
+    console.log(`Keeper running with limit ${taskArguments.limit} on queue(s) ${queues}`)
 
     while(true) {
 
-        console.log(`Checking for upkeep...`);
-        const subscriptionManager = await ethers.getContract("CaskSubscriptionManager");
-        const checkData = ethers.utils.defaultAbiCoder.encode(
-            ['uint256','uint256','uint8'],
-            [
-                parseInt(taskArguments.limit),
-                parseInt(taskArguments.minDepth),
-                parseInt(taskArguments.queue)
-            ]);
-        const checkResult = await subscriptionManager.checkUpkeep(checkData);
-        if (checkResult.upkeepNeeded) {
-            console.log(`Upkeep needed. Performing upkeep...`);
-            const txn = await subscriptionManager.performUpkeep(checkResult.performData, {gasLimit: 2600000});
-            console.log(`Upkeep complete: txn ${txn.hash}`);
-        } else {
-            console.log(`No upkeep needed.`);
+        for (let i = 0; i < queues.length; i++) {
+            const queue = queues[i];
+            console.log(`Checking for upkeep on queue ${queue}...`);
+            const subscriptionManager = await ethers.getContract("CaskSubscriptionManager");
+            const checkData = ethers.utils.defaultAbiCoder.encode(
+                ['uint256','uint256','uint8'],
+                [
+                    parseInt(taskArguments.limit),
+                    parseInt(taskArguments.minDepth),
+                    parseInt(queue)
+                ]);
+            const checkEstimatedGas = await subscriptionManager.estimateGas.checkUpkeep(checkData);
+            if (checkEstimatedGas.gt(taskArguments.gasLimit)){
+                console.log(`Warning: estimatedGas for checkUpkeep on queue ${queue} is above gasLimit ${taskArguments.gasLimit}`);
+            }
+            const checkResult = await subscriptionManager.checkUpkeep(checkData);
+            if (checkResult.upkeepNeeded) {
+                console.log(`Upkeep needed on queue ${queue}. Performing upkeep...`);
+                const performEstimatedGas = await subscriptionManager.estimateGas
+                    .performUpkeep(checkResult.performData, {gasLimit: parseInt(taskArguments.gasLimit)});
+                if (performEstimatedGas.gt(taskArguments.gasLimit)){
+                    console.log(`Warning: estimatedGas for performUpkeep on queue ${queue} is above gasLimit ${taskArguments.gasLimit}`);
+                }
+                const txn = await subscriptionManager
+                    .performUpkeep(checkResult.performData, {gasLimit: parseInt(taskArguments.gasLimit)});
+                console.log(`Upkeep complete for queue ${queue}: txn ${txn.hash}`);
+            } else {
+                console.log(`No upkeep needed on queue ${queue}.`);
+            }
         }
 
         await new Promise(r => setTimeout(() => r(), parseInt(taskArguments.interval)));
