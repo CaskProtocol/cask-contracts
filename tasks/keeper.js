15081b0fb6da3d985b6b9a51bc459c120bf95bc3
mike <masha256bit@protonmail.com>
2022-05-11 22:40:32 -0700

    keeper error handling

--- tasks/keeper.js
+++ tasks/keeper.js
@@ -21,46 +21,50 @@ async function keeper(taskArguments, hre) {
     while(true) {
 
         for (let i = 0; i < queues.length; i++) {
-            const queue = queues[i];
-            console.log(`Checking for upkeep on queue ${queue}...`);
+            try {
+                const queue = queues[i];
+                console.log(`Checking for upkeep on queue ${queue}...`);
 
-            const checkData = ethers.utils.defaultAbiCoder.encode(
-                ['uint256','uint256','uint8'],
-                [
-                    parseInt(taskArguments.limit),
-                    parseInt(taskArguments.minDepth),
-                    parseInt(queue)
-                ]);
-            const checkEstimatedGas = await keeperManager.estimateGas.checkUpkeep(checkData);
-            if (checkEstimatedGas.gt(taskArguments.gasLimit)){
-                console.log(`Warning: estimatedGas for checkUpkeep on queue ${queue} is above gasLimit ${taskArguments.gasLimit}`);
-            }
-            const checkResult = await keeperManager.checkUpkeep(checkData);
-            if (checkResult.upkeepNeeded) {
-                console.log(`Upkeep needed on queue ${queue}. Performing upkeep...`);
-                const performEstimatedGas = await keeperManager.estimateGas
-                    .performUpkeep(checkResult.performData, {
-                        gasLimit: parseInt(taskArguments.gasLimit),
-                        gasPrice: parseInt(taskArguments.gasPrice)
-                    });
-                if (performEstimatedGas.gt(taskArguments.gasLimit)){
-                    console.log(`Warning: estimatedGas for performUpkeep on queue ${queue} is above gasLimit ${taskArguments.gasLimit}`);
+                const checkData = ethers.utils.defaultAbiCoder.encode(
+                    ['uint256', 'uint256', 'uint8'],
+                    [
+                        parseInt(taskArguments.limit),
+                        parseInt(taskArguments.minDepth),
+                        parseInt(queue)
+                    ]);
+                const checkEstimatedGas = await keeperManager.estimateGas.checkUpkeep(checkData);
+                if (checkEstimatedGas.gt(taskArguments.gasLimit)) {
+                    console.log(`Warning: estimatedGas for checkUpkeep on queue ${queue} is above gasLimit ${taskArguments.gasLimit}`);
                 }
-                const tx = await keeperManager
-                    .performUpkeep(checkResult.performData, {
-                        gasLimit: parseInt(taskArguments.gasLimit),
-                        gasPrice: parseInt(taskArguments.gasPrice)
-                    });
-                const events = (await tx.wait()).events || [];
-                const report = events.find((e) => e.event === "SubscriptionManagerReport");
-                if (report) {
-                    console.log(`Report: performed ${report.args.renewals} renewals`);
+                const checkResult = await keeperManager.checkUpkeep(checkData);
+                if (checkResult.upkeepNeeded) {
+                    console.log(`Upkeep needed on queue ${queue}. Performing upkeep...`);
+                    const performEstimatedGas = await keeperManager.estimateGas
+                        .performUpkeep(checkResult.performData, {
+                            gasLimit: parseInt(taskArguments.gasLimit),
+                            gasPrice: parseInt(taskArguments.gasPrice)
+                        });
+                    if (performEstimatedGas.gt(taskArguments.gasLimit)) {
+                        console.log(`Warning: estimatedGas for performUpkeep on queue ${queue} is above gasLimit ${taskArguments.gasLimit}`);
+                    }
+                    const tx = await keeperManager
+                        .performUpkeep(checkResult.performData, {
+                            gasLimit: parseInt(taskArguments.gasLimit),
+                            gasPrice: parseInt(taskArguments.gasPrice)
+                        });
+                    const events = (await tx.wait()).events || [];
+                    const report = events.find((e) => e.event === "SubscriptionManagerReport");
+                    if (report) {
+                        console.log(`Report: performed ${report.args.renewals} renewals`);
+                    } else {
+                        console.log(`Report not detected after keeper run`);
+                    }
+                    console.log(`Upkeep complete for queue ${queue}: txn ${tx.hash}`);
                 } else {
-                    console.log(`Report not detected after keeper run`);
+                    console.log(`No upkeep needed on queue ${queue}.`);
                 }
-                console.log(`Upkeep complete for queue ${queue}: txn ${tx.hash}`);
-            } else {
-                console.log(`No upkeep needed on queue ${queue}.`);
+            } catch (err) {
+                console.log(`Keeper error: ${err.toString()}`);
             }
         }
 
